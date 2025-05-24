5200c4b63d35268e5ac153fb0b0d163258beb8f9
Sourabh <rautsourabh3@gmail.com>
2025-05-24 08:10:02 +0530

    sourabh has modified app.js added console.log etc

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 const heading = React.createElement(
-  "h1",
-  { id: "header", className: "rrr" },
-  "wwHello React"
-);
+    "div",
+    { id: "Parent" },
+    [React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I am H1 tag"),
+      React.createElement("h2", {}, "I am H22 tag"),
+    ]),React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I am H1 tag"),
+        React.createElement("h2", {}, "I am H22 tag"),
+      ]),]
+  ); //this is becoming very complex so JSX came in picture
+  console.log("In-->",heading);//object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
-const heading1 = React.createElement(
-  "div",
-  { id: "Parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am H1 tag"),
-    React.createElement("h2", {}, "I am H2 tag"),
-  ]),
-);
-const root1 = ReactDOM.createRoot(document.getElementById("root1"));
-root1.render(heading1);
 //OR
-// ReactDOM.createRoot(document.getElementById("root1")).render(heading1);
+// ReactDOM.createRoot(document.getElementById("root")).render(heading);
